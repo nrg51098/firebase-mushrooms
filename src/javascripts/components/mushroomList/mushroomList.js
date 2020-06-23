2a866bb8e17211eb4a44c24e91a0e15976f8b50e
Nikhil Gaikwad <gaikwadrnikhil@gmail.com>
2020-06-22 20:42:15 -0500

    added the mushroom

--- src/javascripts/components/mushroomList/mushroomList.js
+++ src/javascripts/components/mushroomList/mushroomList.js
@@ -1,6 +1,7 @@
 import mushroomData from '../../helpers/data/mushroomData';
+import mushroomComponent from '../mushroom/mushroom';
 
-// import utils from '../../helpers/utils';
+import utils from '../../helpers/utils';
 
 // get all mushrooms
 // loop and create domstring
@@ -8,7 +9,19 @@ import mushroomData from '../../helpers/data/mushroomData';
 
 const buildForest = () => {
   mushroomData.getMushrooms()
-    .then((response) => console.warn('Get Mushrooms worked!!', response.data))
+    .then((mushrooms) => {
+      console.warn('Get mushrooms worked', mushrooms);
+      let domString = `
+      <h2 class="text-center">Forest</h2>
+      <div class="d-flex flex-wrap">
+      `;
+      mushrooms.forEach((mushroom) => {
+        domString += mushroomComponent.mushroomMaker(mushroom);
+      });
+      domString += '</div>';
+
+      utils.printToDom('#forest', domString);
+    })
     .catch((err) => console.error('get mushrooms broke :/', err));
   // const domString = '<h1>It does actually work</h1>';
   // utils.printToDom('#forest', domString);
